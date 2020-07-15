d783c95329d57f608f14aa949f5cc7438cd8f632
Duncan Calder <DCalder@clarku.edu>
2020-07-15 18:10:07 -0400

    Playing around with form code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <NoteForm />
+            </header>
+        </div>
+    );
 }
+
+class NoteForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            topic: '',
+            description: '',
+            college: '',
+            course: '',
+            file: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ 
+            topic: event.target.topic,
+            description: event.target.description,
+            college: event.target.colege,
+            course: event.target.course,
+            file: event.target.file
+        });
+    }
+
+    handleSubmit(event) {
+        alert('A form was submitted: ' + this.state.topic);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Note:
+                    <input type="file" />
+                </label>
+
+                <label>
+                    Topic:
+                    <input type="text" value={this.state.topic} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    Description:
+                    <input type="text" value={this.state.description} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    College Name:
+                    <input type="text" value={this.state.college} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    Course Name:
+                    <input type="text" value={this.state.course} onChange={this.handleChange} />
+                </label>
+        
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <NoteForm />,
+    document.getElementById('root')
+);
 
 export default App;
